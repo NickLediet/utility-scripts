7f33e5e4af57486197768e50216ae8493eacab0e
nicholas <nlediet@northern.co>
2018-10-24 09:49:24 -0400

    Add docker install and config script

---
+++ docker-install.sh
@@ -0,0 +1,19 @@
+# !/bin/bash
+
+# Download/Prepare
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+apt-cache policy docker-ce
+
+# Install
+sudo apt-get install -y docker-ce
+sudo systemctl status docker
+
+# Config/Set privs
+sudo usermod -aG docker ${USER}
+su - ${USER}
+id -nG
+
+# Test
+docker --version
